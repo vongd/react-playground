cd255e8c511bf22cb74f5a32b3a85f4ab5273f51
joshuaegclark <joshuaegclark@gmail.com>
2016-07-06 08:50:37 -0600

    Non-flex and flex Material Card mockup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import './index.scss';
 
 class App extends Component {
 
   render() {
     return (
-      <h1>This is a thing</h1>
+      <div className="outer-container">
+        <div className="container">
+          <div className="header"></div>
+          <div className="card">
+            <div className="card-title">
+              <h2 className="card-title-text">Things!</h2>
+            </div>
+            <div className="card-supporting-text">
+             This is the description of my card. It tells you
+             a little about the main content.
+            </div>
+            <div className="card-actions">
+              <a className="button action-button ripple-effect">
+                Do things
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex-container">
+          <div className="flex-card">
+            <div className="flex-card-title">
+              <h2 className="flex-card-title-text">Things!</h2>
+            </div>
+            <div className="flex-card-supporting-text">
+             This is the description of my card. It tells you
+             a little about the main content.
+            </div>
+            <div className="flex-card-actions">
+              <a className="flex-button flex-action-button flex-ripple-effect">
+                Do things
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
