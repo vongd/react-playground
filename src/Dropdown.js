6d1e2535f64afa9b88787e6c8215f410295e50ad
David Vong <dvong@rallydev.com>
2016-09-14 15:38:26 -0600

    Edited Dropdown files js and scss and got rid of link underlines

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './Dropdown.scss';
 
 
-var Dropdown = React.creatClass({
+var Dropdown = React.createClass({
   getIntitalState: function() {
     return {
       listVisible: false
     };
   },
+
+  select: function(item) {
+
+  },
+
+  show: function() {
+
+  },
+
+  hide: function() {
+
+  },
+
+  render: function(){
+    <div>Hello</div>
+  }
 
 })
 
