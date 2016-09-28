e6f35102c5a839bf6578cd288dc1add5f65c08b4
David Vong <dvong@rallydev.com>
2016-09-28 15:30:24 -0600

    Added Normalize and made more changes to the header

--- src/index.js
+++ src/index.js
@@ -31,20 +31,20 @@ var HeaderBar = React.createClass({
   render: function() {
     return (
       <div className="headerBar">
-      HEADER THIS STUFF
-        <ul className="left-side">
+        <span className="title">
+          GOTVONG
+        </span>
+        <ul className="right-side">
   				<li><a href="#">BRAND</a></li>
   				<li><a href="/Services">SERVICES</a></li>
-  				<ul className="dropdown">
-  					<li><a href="#">Weddings</a></li>
-  					<li><a href="#">Portraits</a></li>
-  				</ul>
-  				<ul className="sub-menu2">
-  					<li><a href="#">School</a></li>
-  					<li><a href="#">Family</a></li>
-  				</ul>
-  			</ul>
-  			<ul className="right-side">
+    				{/* <ul className="dropdown">
+    					<li><a href="#">Weddings</a></li>
+    					<li><a href="#">Portraits</a></li>
+    				</ul>
+    				<ul className="sub-menu2">
+    					<li><a href="#">School</a></li>
+    					<li><a href="#">Family</a></li>
+    				</ul> */}
   				<li><a href="#">SIGN UP</a></li>
   				<li><a href="#">CONTACT</a></li>
   				<li><a href="#">LOG IN</a></li>
